10c8731a1b3e7580c3da5b2e7455fffd54bf2beb
jose <jose@jose-K52F>
2015-11-24 00:38:58 +0100

    jsonmaker con formulario

--- src/main/webapp/js/examen.js
+++ src/main/webapp/js/examen.js
@@ -5,21 +5,56 @@
  */
 
 
-var path = 'request.getContextPath()';
-
 $(document).ready(function () {
-  
-  
-  $.ajax({
+
+
+    var numero = prompt("Introduce un numero");
+    var path = "http://localhost:8081/examClientNov2014/control?ob=form&op=get&num=" + numero;
+    $.ajax({
         url: path,
         type: "GET",
         async: false,
         dataType: "json",
-        success: function (source) {
-             /* res += source.json; */
-             $('#data').add(source.Control);
-             /* $('#data').append(source.Control); */ 
+        success: function (json) {
+
+            var texto = JSON.stringify(json);
+            $('#paneljson').append(texto);
+
+            
+
+            mijson = JSON.parse(texto);
+            var message = '<form>';
+            
+            for (var i = 0; i < mijson.json.elements.length; i++) {
+                
+                if (mijson.json.elements[i].text === undefined) {
+                   
+                    message += '<' + mijson.json.elements[i].tag;
+                    message += ' type="' + mijson.json.elements[i].type + '"';
+                    message += ' name="' + mijson.json.elements[i].name + '"';
+                    message += ' id="' + mijson.json.elements[i].id + '"' + '>';
+                    message += '</' + mijson.json.elements[i].tag + '> <br>';
+                    
+                } else {
+                    
+                    message += '<' + mijson.json.elements[i].tag;
+                    message += ' for="' + mijson.json.elements[i].labelfor + '">';
+                    message +=  mijson.json.elements[i].text;
+                    message += '</' + mijson.json.elements[i].tag + '>';
+                }
+                
+                
+            }
+
+            message += '</form>';
+            
+           $('#formulario').append(message);
+
+            /* $('#data').append(source.Control); */
+
+        },
+        error: function (dato) {
+
         }
-        
     });
-});
+});   
